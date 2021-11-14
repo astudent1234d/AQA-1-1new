4a8bc0b952883df60dd99067037d370614ce691c
user <test@>
2021-11-14 15:46:23 +0300

    Добавлен .gitignore, текст программы

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void remain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+}
